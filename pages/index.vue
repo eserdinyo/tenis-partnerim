0b3f9882df5f428aedaf99c65559efe856cbdcc0
Muhammet_ESER <muhammeteser29@gmail.com>
2021-06-22 15:18:57 +0300

    fix: homepage grid

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,14 @@
     <Loader v-if="$fetchState.pending" />
     <div
       v-else
-      class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-6 mb-8"
+      class="
+        grid grid-cols-1
+        md:grid-cols-2
+        lg:grid-cols-3
+        xl:grid-cols-4
+        gap-6
+        mb-8
+      "
     >
       <Player v-for="player in players" :key="player.slug" :player="player" />
     </div>
